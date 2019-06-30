b14bac57e21df8f35092654a1ef710cbe1dc6e9a
minggas <fasfora@gmail.com>
2019-06-30 14:11:21 -0300

    fix: add thread_id field to reply model

--- models/Reply.js
+++ models/Reply.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const ReplySchema = new mongoose.Schema({
+  thread_id: { type: mongoose.Schema.Types.ObjectId, ref: "Thread" },
   text: {
     type: String,
     required: true,
